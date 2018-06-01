7a4e009a1d018984c1ea0b1ce24b2040d5edc15d
Артём <hoodsey20@gmail.com>
2018-06-02 01:49:42 +0700

    обработчик нажатия кнопки «назад» для возвращения на интро-экран

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ import {renderScreen} from './util.js';
 import introScreen from './intro-screen.js';
 
 renderScreen(introScreen);
+
+document.addEventListener(`click`, (evt) => {
+  const backButton = document.querySelector(`.header__back .back`);
+  if (evt.target === backButton || evt.target.parentNode === backButton) {
+    renderScreen(introScreen);
+  }
+});
