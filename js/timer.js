7ba6fcf617dbdc1c544c246c844105f72872c789
Артём <hoodsey20@gmail.com>
2018-06-08 14:41:40 +0700

    переписал таймер, удалил тесты для метода таймера isActive т.к. его больше нет

--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,3 @@
-const Timer = function (time) {
-  this.time = time;
-  this.tick = function () {
-    if (this.time > 0) {
-      this.time--;
-    }
-
-    return this;
-  };
-  this.isActive = function () {
-    return !!this.time;
-  };
-
-  return this;
-};
-
 export const createTimer = (time) => {
   if (typeof time !== `number`) {
     throw new Error(`Time should be of type number`);
@@ -23,7 +7,12 @@ export const createTimer = (time) => {
     throw new Error(`Time should not be 0 or negative value`);
   }
 
-  const timer = new Timer(parseInt(time, 10));
-  const timerCopy = Object.assign({}, timer);
-  return timerCopy;
+  const currentTime = parseInt(time, 10);
+
+  return Object.freeze({
+    time: currentTime,
+    tick() {
+      return createTimer(currentTime - 1);
+    },
+  });
 };
--- js/timer.test.js
+++ js/timer.test.js
@@ -7,36 +7,28 @@ describe(`Timer`, () => {
     assert.throws(() => createTimer(`2`), /Time should be of type number/);
   });
 
-  it(`should not allow set negative values of time`, () => {
+  it(`should not allow set negative values of time or 0`, () => {
     assert.throws(() => createTimer(0), /Time should not be 0 or negative value/);
     assert.throws(() => createTimer(-1), /Time should not be 0 or negative value/);
     assert.throws(() => createTimer(-10), /Time should not be 0 or negative value/);
   });
 
   it(`should return integer passed number of time`, () => {
-    assert.equal(createTimer(1).time, 1);
+    assert.equal(createTimer(1.1).time, 1);
     assert.equal(createTimer(30).time, 30);
     assert.equal(createTimer(30.349234).time, 30);
-    assert.equal(createTimer(9999).time, 9999);
+    assert.equal(createTimer(9999.99).time, 9999);
   });
 
   it(`should subtract one when tick() was fired`, () => {
     assert.equal(createTimer(2).tick().time, 1);
     assert.equal(createTimer(10).tick().time, 9);
+    assert.equal(createTimer(100).tick().time, 99);
   });
 
   it(`should subtract two when tick() was double-fired`, () => {
     assert.equal(createTimer(10).tick().tick().time, 8);
-  });
-
-  it(`should return true active status when time is not up`, () => {
-    assert.equal(createTimer(1).isActive(), true);
-    assert.equal(createTimer(2).tick().isActive(), true);
-    assert.equal(createTimer(10).isActive(), true);
-  });
-
-  it(`should return false active status when time is up`, () => {
-    assert.equal(createTimer(1).tick().isActive(), false);
-    assert.equal(createTimer(2).tick().tick().isActive(), false);
+    assert.equal(createTimer(3).tick().tick().time, 1);
+    assert.equal(createTimer(99).tick().tick().time, 97);
   });
 });
