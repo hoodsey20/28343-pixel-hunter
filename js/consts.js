2b7fe9a702ae268282b596510e22ea178d43b412
Артём <hoodsey20@gmail.com>
2018-06-11 02:03:38 +0700

    вынес константы в отдельный файл

---
+++ js/consts.js
@@ -0,0 +1,13 @@
+export const answerStatus = {
+  UNKNOWN: `UNKNOWN`,
+  WRONG: `WRONG`,
+  SLOW: `SLOW`,
+  FAST: `FAST`,
+  CORRECT: `CORRECT`
+};
+
+export const timerConditions = {
+  FAST: 10,
+  SLOW: 20,
+  MAX: 30,
+};
