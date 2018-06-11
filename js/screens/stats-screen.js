4e67ead5dbe0ae58d623ae3986b9f697444e384b
Артём <hoodsey20@gmail.com>
2018-06-11 15:21:19 +0700

    разделил шаблон вывода статистики на модули

--- js/screens/stats-screen.js
+++ js/screens/stats-screen.js
@@ -1,81 +1,18 @@
 import {getElementFromTemplate} from './../util';
 import {headerTemplate} from './../chunks/header';
-import {statsTemplate} from './../chunks/stats';
 import {footerTemplate} from './../chunks/footer';
-import {getFinalPoints} from './../get-final-points';
+import {successResultTemplate} from './../chunks/success-result';
+import {failResultTemplate} from './../chunks/fail-result';
 
 const statsScreen = (data, status) => {
   const currentData = data;
   const title = status ? `Победа!` : `Поражение :(`;
-  const result = status ? getFinalPoints(currentData.answers, currentData.lifes) : `FAIL`;
+  const resultTemplate = status ? successResultTemplate : failResultTemplate;
 
   const content = `${headerTemplate(currentData)}
   <div class="result">
     <h1>${title}</h1>
-    <table class="result__table">
-      <tr>
-        <td class="result__number">1.</td>
-        <td colspan="2">
-          ${statsTemplate(currentData.answers)}
-        </td>
-        <td class="result__points">×&nbsp;100</td>
-        <td class="result__total">900</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Бонус за скорость:</td>
-        <td class="result__extra">1&nbsp;<span class="stats__result stats__result--fast"></span></td>
-        <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">50</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Бонус за жизни:</td>
-        <td class="result__extra">2&nbsp;<span class="stats__result stats__result--alive"></span></td>
-        <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">100</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Штраф за медлительность:</td>
-        <td class="result__extra">2&nbsp;<span class="stats__result stats__result--slow"></span></td>
-        <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">-100</td>
-      </tr>
-      <tr>
-        <td colspan="5" class="result__total  result__total--final">${result}</td>
-      </tr>
-    </table>
-    <table class="result__table">
-      <tr>
-        <td class="result__number">2.</td>
-        <td>
-          ${statsTemplate(currentData.answers)}
-        </td>
-        <td class="result__total"></td>
-        <td class="result__total  result__total--final">fail</td>
-      </tr>
-    </table>
-    <table class="result__table">
-      <tr>
-        <td class="result__number">3.</td>
-        <td colspan="2">
-          ${statsTemplate(currentData.answers)}
-        </td>
-        <td class="result__points">×&nbsp;100</td>
-        <td class="result__total">900</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Бонус за жизни:</td>
-        <td class="result__extra">2&nbsp;<span class="stats__result stats__result--alive"></span></td>
-        <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">100</td>
-      </tr>
-      <tr>
-        <td colspan="5" class="result__total  result__total--final">950</td>
-      </tr>
-    </table>
+    ${resultTemplate(1, currentData.answers, currentData.lifes)}
   </div>${footerTemplate}`;
 
   const element = getElementFromTemplate(content);
