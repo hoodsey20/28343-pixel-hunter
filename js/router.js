83f8bff36d5addd6450f5f52e0a6d5d12bda1069
Артём <hoodsey20@gmail.com>
2018-06-27 20:14:05 +0700

    реализовал настоящие данные с сервера через адаптер

--- js/router.js
+++ js/router.js
@@ -3,8 +3,10 @@ import GreetingPresenter from './presenter/greeting-presenter';
 import RulesPresenter from './presenter/rules-presenter';
 import GamePresenter from './presenter/game-presenter';
 import StatisticsPresenter from './presenter/statistics-presenter';
+import ModalErrorView from './view/modal-error';
 
 import GameModel from './game-model';
+import {dataAdapter} from './data-adapter';
 
 const rootNode = document.querySelector(`.central`);
 
@@ -13,10 +15,35 @@ const changeView = (element) => {
   rootNode.appendChild(element);
 };
 
+const QUESTIONS_URL = `https://es.dump.academy/pixel-hunter/questions`;
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+let gameQuestionsData = null;
+
 export default class Router {
   static showIntro() {
     const introScreen = new IntroPresenter();
     changeView(introScreen.content.element);
+    window.fetch(QUESTIONS_URL)
+      .then(checkStatus)
+      .then((response) => response.json())
+      .then((data) => {
+        gameQuestionsData = dataAdapter(data, true);
+        return gameQuestionsData;
+      })
+      .then(() => this.showGreeting())
+      .catch((err) => this.showError(err));
+  }
+
+  static showError(error) {
+    rootNode.appendChild(new ModalErrorView(error).element);
   }
 
   static showGreeting() {
@@ -31,7 +58,7 @@ export default class Router {
 
   static showGame(playerName) {
     rootNode.innerHTML = ``;
-    const gameModel = new GameModel(playerName);
+    const gameModel = new GameModel(playerName, gameQuestionsData);
     gameModel.questionNumber = 1;
     const gameScreen = new GamePresenter(gameModel);
     gameScreen.startGame();
