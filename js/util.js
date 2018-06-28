1ff50c65aa6adec7107eef9b7d0a22dc7038190c
Артём <hoodsey20@gmail.com>
2018-06-28 23:48:36 +0700

    Б19. Нет файлов скриптов, которые являются «мёртвым кодом»

--- js/util.js
+++ js/util.js
@@ -1,14 +1,3 @@
-export const renderScreen = (screenContent) => {
-  const mainScreen = document.querySelector(`.central`);
-  mainScreen.innerHTML = ``;
-  mainScreen.appendChild(screenContent);
-};
-
-export const updateView = (container, view) => {
-  container.innerHTML = ``;
-  container.appendChild(view);
-};
-
 export const render = (html) => {
   const wrapper = document.createElement(`div`);
   wrapper.innerHTML = html.trim();
